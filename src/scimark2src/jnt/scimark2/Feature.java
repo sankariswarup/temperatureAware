3d3a6b6cd0a002b81da0ad46729e25d74d265f15
Sankari Swaroop Anupindi <sza0033@auburn.edu>
2014-05-05 19:35:16 -0500

    Revert "Revert "Thermal Management R1""
    This reverts commit 816a60e9084785a57bf34cd710e9c3011247d27c.

---
+++ src/scimark2src/jnt/scimark2/Feature.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scimark2src.jnt.scimark2;
+
+/**
+ *
+ * @author sza0033
+ */
+public class Feature {
+    public Feature()
+    {
+        
+    }
+    public int value=0;
+    
+}
